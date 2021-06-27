fa30f48ae179749cbfcd46f59fb3c05bc481aeb2
egy186 <egy186@yahoo.co.jp>
2021-06-27 18:32:33 +0900

    execute script only when status === complete

--- src/background.js
+++ src/background.js
@@ -1,30 +1,32 @@
 import { initialOptions } from './constant';
 
 browser.tabs.onUpdated.addListener(async (id, changeInfo, tab) => {
-  // Restore options
-  const {
-    fitHeight,
-    list,
-    scrollAnimation,
-    scrollToFirst
-  } = await browser.storage.sync.get(initialOptions);
-  const { selector } = list.find(config => new RegExp(config.pattern, 'u').test(tab.url)) || {};
-
-  if (selector) {
-    if (fitHeight) {
-      // Append style element
-      const css = `${selector} { max-height: 100vh; width: auto; }`;
-      browser.tabs.insertCSS({ code: css });
-    }
-    await browser.tabs.executeScript({
-      file: 'scroll-to-image.js',
-      runAt: 'document_idle'
-    });
-    browser.tabs.sendMessage(tab.id, {
+  if (changeInfo.status === 'complete') {
+    // Restore options
+    const {
+      fitHeight,
+      list,
       scrollAnimation,
-      scrollToFirst,
-      selector
-    });
+      scrollToFirst
+    } = await browser.storage.sync.get(initialOptions);
+    const { selector } = list.find(config => new RegExp(config.pattern, 'u').test(tab.url)) || {};
+
+    if (selector) {
+      if (fitHeight) {
+        // Append style element
+        const css = `${selector} { max-height: 100vh; width: auto; }`;
+        browser.tabs.insertCSS({ code: css });
+      }
+      await browser.tabs.executeScript({
+        file: 'scroll-to-image.js',
+        runAt: 'document_idle'
+      });
+      browser.tabs.sendMessage(tab.id, {
+        scrollAnimation,
+        scrollToFirst,
+        selector
+      });
+    }
   }
 });
 
